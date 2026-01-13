6728fa0d3ee8a8a44bbeeb94ae68c708a0467dbd
Weldys Andrade do Carmo <weldyscarmo27@gmail.com>
2026-01-13 10:49:22 -0300

    Criando projeto

---
+++ src/main/java/br/com/weldyscarmo/agendamento_consultas_medicas/AgendamentoConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package br.com.weldyscarmo.agendamento_consultas_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoConsultasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weldyscarmo/agendamento_consultas_medicas/AgendamentoConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weldyscarmo.agendamento_consultas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
